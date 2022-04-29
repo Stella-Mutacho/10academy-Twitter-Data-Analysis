6cc4b4e36c54102f21c69bc051e839558603ed99
Stella <stellamutacho@gmail.com>
2022-04-29 18:26:09 +0300

    changes

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,65 +1,59 @@
 import pandas as pd
-
-class CleanTweets:
+class Clean_Tweets:
     """
-    This class is responsible for cleaning the twitter dataframe
-
-    Returns:
-    --------
-    A dataframe
+    The PEP8 Standard AMAZING!!!
     """
     def __init__(self, df:pd.DataFrame):
         self.df = df
         print('Automation in Action...!!!')
         
-    def drop_unwanted_column(self)->pd.DataFrame:
+    def drop_unwanted_column(self, df:pd.DataFrame)->pd.DataFrame:
         """
         remove rows that has column names. This error originated from
         the data collection stage.  
         """
-        unwanted_rows = self.df[self.df['retweet_count'] == 'retweet_count' ].index
-        self.df.drop(unwanted_rows , inplace=True)
-        self.df = self.df[self.df['polarity'] != 'polarity']
+        unwanted_rows = df[df['retweet_count'] == 'retweet_count' ].index
+        df.drop(unwanted_rows , inplace=True)
+        df = df[df['polarity'] != 'polarity']
         
-        return self.df
+        return df
     def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
         """
         
-        self.df = self.df.drop_duplicates().drop_duplicates(subset='original_text')
-        
+        df.drop_duplicates(subset = ['created_at', 'source', 'original_text', 'polarity',
+                                    'subjectivity', 'lang', 'favorite_count', 'retweet_count',
+                                    'original_author','followers_count', 'friends_count', 'possibly_sensitive',
+                                    'place'], inplace=True
+                        )
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        self.df['created_at'] = pd.to_datetime(self.df['created_at'], errors='coerce')
+        df["created_at"] = df["created_at"].astype('datetime64[ns]') 
+        df["created_at"] = df.created_at.dt.to_pydatetime()
         
-        self.df = self.df[self.df['created_at'] >= '2020-12-31' ]
+        df = df[df['created_at'] >= '2020-12-31' ]
         
-        return self.df
+        return df
     
-    def convert_to_numbers(self)->pd.DataFrame:
+    def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        self.df['polarity'] = pd.to_numeric(self.df['polarity'], errors='coerce')
-        self.df['retweet_count'] = pd.to_numeric(self.df['retweet_count'], errors='coerce')
-        self.df['favorite_count'] = pd.to_numeric(self.df['favorite_count'], errors='coerce')
-        
-        return self.df
+        df['polarity']=pd.to_numeric(df.polarity)
+        df['subjectivity']=pd.to_numeric(df.subjectivity)
+        df['retweet_count']=pd.to_numeric(df.retweet_count)
+        df['favorite_count']=pd.to_numeric(df.favorite_count)
+        df['followers_count']=pd.to_numeric(df.followers_count)                
+        return df
     
-    def remove_non_english_tweets(self)->pd.DataFrame:
+    def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
         """
         remove non english tweets from lang
         """
-        
-        self.df = self.df.query("lang == 'en' ")
-        
-        return self.df
-
-if __name__ == "__main__":
-    tweet_df = pd.read_csv("../data/covid19.csv")
-    cleaner = CleanTweets(tweet_df)
+        df = df[df['lang'] == 'en']
+        return df
--- extract_dataframe.py
+++ extract_dataframe.py
@@ -39,13 +39,9 @@ class TweetDfExtractor:
         return statuses_count
         
     def find_full_text(self)->list:
-        text = []
-        for tweet in self.tweets_list:
-            if 'retweeted_status' in tweet.keys() and 'extended_tweet' in tweet['retweeted_status'].keys():
-                text.append(tweet['retweeted_status']['extended_tweet']['full_text'])
-            else: text.append('Empty')
+        
 
-        return text       
+        return [x['text'] for x in self.tweets_list]       
     
     def find_sentiments(self, text:list)->list:
         polarity, subjectivity = [], []
